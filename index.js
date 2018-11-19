de0d2120933eff54bf98c5de986502ed50749314
Kian Edalat <akedalat@gmail.com>
2018-11-19 12:03:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = [];
   for (var i = 0; i < arr.length; i++) {
-    if (i = 0) {
-      arr.push("John Lennon plays guitar")
-    } else {
-arr.push(musicians, instruments)
-    }
-  }
+  case i = 0:
+    arr.push (musicians[0], ' plays ', instruments[0])
+    break;
+    case i = 1:
+      arr.push (musicians[1], ' plays ', instruments[1])
+      break;
+      case i = 2:
+        arr.push (musicians[2], ' plays ', instruments[2])
+        break;
+      break;
   return arr
 }
