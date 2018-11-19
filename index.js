d2e8034809ed9e9e4153f7c7a13ecc0cbd2b48f7
Kian Edalat <akedalat@gmail.com>
2018-11-19 12:06:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = [];
   for (var i = 0; i < arr.length; i++) {
-  case i = 0:
-    arr.push (musicians[0], ' plays ', instruments[0])
-    break;
-    case i = 1:
-      arr.push (musicians[1], ' plays ', instruments[1])
-      break;
-      case i = 2:
-        arr.push (musicians[2], ' plays ', instruments[2])
-        break;
-  return arr
+  
+    arr.push (musicians[i], ' plays ', instruments[i])
+  
 }
