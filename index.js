39a79bc8881a102546a1df5d23284ee144e6b3e4
Kian Edalat <akedalat@gmail.com>
2018-11-19 12:36:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ i++;
   }
 return facts
 }
+
+function iLoveTheBeatles (number){
+var array = [];
+do {
+
+  array.push ("I love the Beatles!")
+  number++;
+} while (number < 15);  
+return array;
+}
