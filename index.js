760fb7aa81077c32125ab95250e2c0ff09590542
Kian Edalat <akedalat@gmail.com>
2018-11-19 12:07:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = [];
   for (var i = 0; i < arr.length; i++) {
-  
+
     arr.push (musicians[i], ' plays ', instruments[i])
-  
+}
+return arr
 }
